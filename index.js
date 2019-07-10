ebfe0db056ea6c9aeed09222e1894f4ce0dfa080
Daniel Santos <tkdsantos@gmail.com>
2019-07-09 21:47:56 -0300

    criado a funcao para criar usuarios e listar usuarios

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
+const projects = [];
+
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+server.post("/projects", (req, res) => {
+  const { id, title, tasks } = req.body;
+
+  projects.push(id, title, tasks);
+
+  return res.json(projects);
+});
+
 server.listen(3000);
